43f6f095fbb991ea23428c7a3e46f050a0c9c52f
Dai1678 <dai7774@gmail.com>
2019-04-24 23:16:03 +0900

    Repository層周りを少し使いやすくした

--- app/src/main/java/com/justdoit/yoda/api/FakeApiService.kt
+++ app/src/main/java/com/justdoit/yoda/api/FakeApiService.kt
@@ -6,10 +6,6 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface FakeApiService {
-    companion object {
-        const val API_URL = "https://jsonplaceholder.typicode.com"
-    }
-
     @GET("posts/{userId}")
     fun getPosts(@Path("userId") id: Int): Call<FakeApiEntity>
 
