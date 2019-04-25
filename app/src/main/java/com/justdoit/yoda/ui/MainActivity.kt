ce3de10e955809b324e8911ef67dfbd6cba6a159
Dai1678 <dai7774@gmail.com>
2019-04-25 22:27:20 +0900

    fcmの導入完了

--- app/src/main/java/com/justdoit/yoda/ui/MainActivity.kt
+++ app/src/main/java/com/justdoit/yoda/ui/MainActivity.kt
@@ -1,38 +1,14 @@
 package com.justdoit.yoda.ui
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.tasks.OnCompleteListener
-import com.google.firebase.iid.FirebaseInstanceId
-import com.google.firebase.messaging.FirebaseMessaging
 import com.justdoit.yoda.R
-import com.justdoit.yoda.api.MyFirebaseMessagingService
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.w("firebase", "getInstanceId failed", task.exception)
-                    return@OnCompleteListener
-                }
-
-                // Get new Instance ID token
-                val token = task.result?.token
-
-                // Log and toast
-                val msg = "InstanceID Token: $token"
-                Log.d("firebase", msg)
-                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-            })
-
-
     }
 
 }
