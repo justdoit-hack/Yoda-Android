b9065d8031726dd51e70f7c235e208cec4f06818
wintu <wintu520@gmail.com>
2019-04-26 04:12:34 +0900

    fix

--- app/src/main/java/com/justdoit/yoda/ui/MainActivity.kt
+++ app/src/main/java/com/justdoit/yoda/ui/MainActivity.kt
@@ -1,26 +1,14 @@
 package com.justdoit.yoda.ui
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.justdoit.yoda.R
-import com.justdoit.yoda.repository.UserRepository
-import com.justdoit.yoda.utils.exec
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val userRepository = UserRepository.getInstance()
-
-        val phoneNumber = "08067788700"
-        val password = "test1234"
-        
     }
 
 }
