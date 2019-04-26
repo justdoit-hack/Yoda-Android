aaa9a7decbe3f54eed13cf118153af0c9f7b1d36
Wintu <wintu520@gmail.com>
2019-04-26 16:00:03 +0900

    make Application Class

--- app/src/main/java/com/justdoit/yoda/SessionManager.kt
+++ app/src/main/java/com/justdoit/yoda/SessionManager.kt
@@ -2,28 +2,29 @@ package com.justdoit.yoda
 
 import android.content.Context
 import com.justdoit.yoda.entity.User
-import com.justdoit.yoda.ui.MainActivity
 
-class SessionManager private constructor(activity: MainActivity) {
+class SessionManager private constructor() {
     private val authTokenKey = "authToken"
-    private val prefs = activity.getPreferences(Context.MODE_PRIVATE)
+    private val appName = "yoda"
+    private val prefs = Yoda.instance.getSharedPreferences(this.appName, Context.MODE_PRIVATE)
 
+    private object Holder {
+        val INSTANCE = SessionManager()
+    }
+
+    companion object {
+        val instance: SessionManager by lazy { Holder.INSTANCE }
+    }
+
+    var user: User? = null
     var authToken: String?
     set(value) {
         this.prefs.edit().putString(this.authTokenKey, value).apply()
     }
     get() {
-        val token = this.prefs.getString(this.authTokenKey, "")
-        return if (token != "") token else null
+        return this.prefs.getString(this.authTokenKey, null)
     }
-    var user: User? = null
 
-    companion object {
-        private var instance: SessionManager? = null
-        fun getInstanc(activity: MainActivity): SessionManager {
-            return this.instance ?: SessionManager(activity)
-        }
-    }
 
     fun login(user: User) {
         this.user = user
@@ -34,6 +35,6 @@ class SessionManager private constructor(activity: MainActivity) {
 
     fun logout() {
         this.user = null
-        this.authToken = ""
+        this.authToken = null
     }
 }
