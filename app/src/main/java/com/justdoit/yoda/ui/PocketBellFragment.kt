ffe38a4d23196bf312902851c534bae1135127bc
TaigaNatto <taigan3253@gmail.com>
2019-04-28 11:46:13 +0900

    fix pocketbell display system

--- app/src/main/java/com/justdoit/yoda/ui/PocketBellFragment.kt
+++ app/src/main/java/com/justdoit/yoda/ui/PocketBellFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.justdoit.yoda.R
+import com.justdoit.yoda.databinding.FragmentPocketBellBinding
 
 class PocketBellFragment : Fragment() {
+
+    lateinit var binding: FragmentPocketBellBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +21,20 @@ class PocketBellFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_pocket_bell,
+            container,
+            false
+        ) as FragmentPocketBellBinding
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pocket_bell, container, false)
+        return binding.root
+    }
+
+    // ここはviewmodel使えばいらなさそう
+    private fun changeDisplay(text: String, colorResource: Int) {
+        binding.displayPocketBell.setBackgroundColor(colorResource)
+        binding.displayPocketBell.text = text
     }
 }
