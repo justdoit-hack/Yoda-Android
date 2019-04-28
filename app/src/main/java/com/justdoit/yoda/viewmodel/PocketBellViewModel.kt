00f71fdbb25d4edc996973a830cee90ae2516ec4
TaigaNatto <taigan3253@gmail.com>
2019-04-28 12:20:50 +0900

    add method for pocketbell left-right btn click

--- app/src/main/java/com/justdoit/yoda/viewmodel/PocketBellViewModel.kt
+++ app/src/main/java/com/justdoit/yoda/viewmodel/PocketBellViewModel.kt
@@ -7,10 +7,37 @@ import androidx.lifecycle.AndroidViewModel
 
 class PocketBellViewModel(application: Application) : AndroidViewModel(application) {
     val statusDisplay: ObservableField<DisplayInfo> = ObservableField()
+    private var page = 0
+
+    val displays = listOf(
+        DisplayInfo("頑張れば不可能は無い", Color.parseColor("#EFEDE4")),
+        DisplayInfo("最強になれ", Color.parseColor("#EFEDE4"))
+    )
 
     init {
         val statusInit = DisplayInfo("カクニンチュウ", Color.parseColor("#EFEDE4"))
         statusDisplay.set(statusInit)
+        page = 0
+    }
+
+    fun clickLeftBtn() {
+        page--
+        if (page < 0) {
+            page = displays.size - 1
+        }
+        changePage(page)
+    }
+
+    fun clickRightBtn() {
+        page++
+        if (page >= displays.size) {
+            page = 0
+        }
+        changePage(page)
+    }
+
+    fun changePage(page: Int) {
+        statusDisplay.set(displays[page])
     }
 
     data class DisplayInfo(
